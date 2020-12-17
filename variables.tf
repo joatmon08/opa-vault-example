ff99074ce435ad6b9b9b05e74128301dbcbace27
Taylor Dolezal <onlydole@users.noreply.github.com>
2020-12-17 13:06:37 -0500

    WIP - Add Kubernetes Cluster for OPA and Vault testing (#1)
    * Add EKS cluster
    
    * Don't use GitHub Actions for Terraform, yet
    
    * Add Vault Helm Chart
    
    * Add Terraform integrations for GitHub Actions
    
    * Use Vault external server
    
    * Mark Vault address as sensitive
    
    Co-authored-by: Rosemary Wang <915624+joatmon08@users.noreply.github.com>

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project_name" {
+  type        = string
+  description = "Globally used project name for this demo."
+  default     = "opa-vault-plugin"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS US-based Region that will be used in this demo."
+  default     = "us-east-1"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zones that will be used in this demo."
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "The Kubernetes version for our clusters"
+  default     = "1.18"
+}
+
+variable "cidr" {
+  type        = string
+  description = "The CIDR block to use for this demo"
+  default     = "10.0.0.0/16"
+}
+
+
+variable "vault_address" {
+  type        = string
+  description = "Address to the Vault Address"
+  sensitive   = true
+}
